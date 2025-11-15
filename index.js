d9c56d2cdf92dd5693b65e1128095909c498db62
KrishDhimanOfficial <krrishdhiman841@gmail.com>
2025-11-15 23:51:55 +0530

    update code

--- index.js
+++ index.js
@@ -42,5 +42,4 @@ app.use('/admin', adminRoutes)
 
 app.use('/', (req, res) => res.render('index'))
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = app
